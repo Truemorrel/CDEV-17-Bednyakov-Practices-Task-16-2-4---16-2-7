4907db8ff1d590510d0c8c05cce1e85a91b40871
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-22 02:50:50 +0300

    Add project files.

---
+++ Task 16 2 4.Tests/CalculatorTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_16_2_4.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void MultiplicationMustReturnNotNullValue()
+        {
+            var result = _calculator.Multiplicator(2, 4);
+            
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void SubstructionMustReturnCorrectValue()
+        {
+            var result = _calculator.Subtraction(5, 4);
+
+            Assert.That(result == 1);
+        }
+
+        [TestCase(3)]
+        [TestCase(5)]
+        public void DevisionMustReturnCorrectVolue(int value)
+        {
+            var result = _calculator.Devision(5, value);
+            Assert.That(result == 1);
+        }
+
+        [TestCase(0)]
+        public void DevisionMustThrowException(int value)
+        {
+            Assert.Throws < DivideByZeroException > (() => _calculator.Devision(5, value));
+        }
+    }
+}
---
+++ Task 16 2 4/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_16_2_4
+{
+    public class Calculator
+    {
+        public int Multiplicator(int a, int b)
+            { return a * b; }
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Devision(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
